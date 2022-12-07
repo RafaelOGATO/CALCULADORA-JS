0cbf561c4ec1e72aa42eaaae5b3615a23cea691b
Rafael O Gato <110108497+RafaelOGATO@users.noreply.github.com>
2022-12-07 09:31:33 -0300

    Add files via upload
    teste de envio

---
+++ index.js
@@ -0,0 +1,25 @@
+//Calculadora com funções em JavaScript
+
+
+console.log(soma(5,8));
+function soma(a,b){
+    return a + b;
+    return a - b;
+    
+}
+
+console.log(subtraçao(10,5));
+function subtraçao(a,b){
+    return a - b;
+}
+
+console.log(divisao(10,5));
+function divisao(a,b){
+    return a / b;
+}
+
+console.log(multiplicaçao(5,8));
+function multiplicaçao(a,b){
+    return a * b;
+}
+console.log(multiplicaçao(10,10));
